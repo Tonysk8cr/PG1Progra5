9d260e788e07d621d3d787756cc2e28d26a53025
Dilan Duran <ddurandiaz3d@gmail.com>
2026-02-24 10:50:30 -0600

    Ultima actualizacion
    Co-Authored-By: yfallas02 <256244432+yfallas02@users.noreply.github.com>

--- Proyecto1/CitasMedicasApp.Client/CitasMedicasApp.Client/wwwroot/js/dashboard.js
+++ Proyecto1/CitasMedicasApp.Client/CitasMedicasApp.Client/wwwroot/js/dashboard.js
@@ -5,22 +5,22 @@
 async function cargarTotales() {
     try {
 
-        // PACIENTES
+        // Se realizan las peticiones a cada endpoint para obtener los datos y luego se actualizan los elementos del dashboard
         const pacientes = await fetch("http://localhost:5003/api/Pacientes/Listar");
         const pacientesData = await pacientes.json();
         document.getElementById("totalPacientes").innerText = pacientesData.length;
 
-        // DOCTORES
+        //En doctores se obtiene la lista de doctores y se actualiza el total de doctores en el dashboard
         const doctores = await fetch("http://localhost:5003/api/Doctores/Listar");
         const doctoresData = await doctores.json();
         document.getElementById("totalDoctores").innerText = doctoresData.length;
 
-        // ESPECIALIDADES
+        // En especialidades se obtiene la lista de especialidades y se actualiza el total de especialidades en el dashboard
         const especialidades = await fetch("http://localhost:5003/api/Especialidades/Listar");
         const especialidadesData = await especialidades.json();
         document.getElementById("totalEspecialidades").innerText = especialidadesData.length;
 
-        // CITAS HOY
+        // En ciatas se obtiene la lista de citas, se filtran las citas del día actual y se actualiza el total de citas del día en el dashboard
         const citas = await fetch("http://localhost:5003/api/Citas/Listar");
         const citasData = await citas.json();
 
--- Proyecto1/CitasMedicasApp.Client/CitasMedicasApp.Client/wwwroot/js/especialidad.js
+++ Proyecto1/CitasMedicasApp.Client/CitasMedicasApp.Client/wwwroot/js/especialidad.js
@@ -21,6 +21,4 @@ async function cargarEspecialidades() {
     });
 }
 
-
-
     cargarEspecialidades();
