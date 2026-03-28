245fc09dec15ac9b56b7d6084c220a1d05336a30
Anthony Villalobos <99383479+Tonysk8cr@users.noreply.github.com>
2026-03-27 21:26:58 -0600

    Proyecto 2 inicio

---
+++ Proyecto2/EstudiantesApp/EstudiantesApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EstudiantesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Proyecto2/EstudiantesApp/EstudiantesApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EstudiantesApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
